5f890394f8a758109a4ac201f57eecf0887cb068
MecseiP <MecseiP@WINDOWS-RMHUEG5>
2023-09-11 10:36:00 +0200

    class created

--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -7,7 +7,23 @@ using System.IO;
 
 namespace balkezesek
 {
-    
+    class Balkezesek
+    {
+        public string Nev { get; set; }
+        public int Elso { get; set; }
+        public int Utolso { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+
+        public Balkezesek(string nev, int elso, int utolso, int suly, int magassag)
+        {
+            Nev = nev;
+            Elso = elso;
+            Utolso = utolso;
+            Suly = suly;
+            Magassag = magassag;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
